fcd6049d16824a09558196b8210efc6225e6f72f
= <=>
2020-02-01 19:14:47 -0800

    add Introduce

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 
     $(window).scroll(function () { 
-        var pos_body =  $('html,body').scrollTop();
-        console.log(pos_body);
-        if(pos_body>=150){
-             $("header").show();
-           
-        }else{
-            $("header").hide();
-        }
+            var pos_body =  $('html,body').scrollTop();
+            console.log(pos_body);
+            if(pos_body>=150){
+                $("header").show();
+            
+            }else{
+                $("header").hide();
+            }
     });
