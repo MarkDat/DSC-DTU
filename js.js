dd929de62d619b1681c95947cca097cc1df48b54
= <=>
2020-02-06 17:44:49 -0800

    add join

--- js.js
+++ js.js
@@ -4,8 +4,29 @@
             console.log(pos_body);
             if(pos_body>=150){
                 $("header").show();
-            
             }else{
                 $("header").hide();
             }
+            
+            showSvgIntro(pos_body);
+            showSvgJoin(pos_body)
     });
+
+    function showSvgIntro(pb){
+        if(pb>=253){
+            $(".display-svg-intro").show();
+        }
+    }
+
+    function showSvgJoin(pb){
+        if(pb>=817){
+            $(".display-svg-join").show();
+        }
+    }
+
+//     $(window).resize(function(){
+
+//         if ($(window).width() <= 1178) {  
+//             // $("#svgIntro").hide(); 
+//         }     
+//  });
--- setting.js
+++ setting.js
@@ -1,7 +1,9 @@
  //ScrollDown introduce
 function scrollToLocation(id){
             let target = $(id).attr("href");
+            console.log("Top: "+$(target).offset().top);
             $('html,body').stop().animate({
             scrollTop: $(target).offset().top
         }, 1000);
-}     
+}   
+
