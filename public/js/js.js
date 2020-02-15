e4d12ea184dc68449c90667125f2c84d1f601711
= <=>
2020-02-14 16:37:18 -0800

    HIHI

--- public/js/js.js
+++ public/js/js.js
@@ -1,6 +1,14 @@
 
     showAll($('html,body').scrollTop());
-  
+//ScrollDown introduce
+function scrollToLocation(id){
+        let target = $(id).attr("href");
+        console.log("Top: "+$(target).offset().top);
+        $('html,body').stop().animate({
+        scrollTop: $(target).offset().top
+        }, 1000);
+}  
+
   $(window).scroll(function () { 
             var pos_body =  $('html,body').scrollTop();
             console.log(pos_body);
@@ -14,14 +22,15 @@
     });
   
     
-
     var posShowJoin =850;
     var posShowTarget =1100;
     var posShowActii = 1562;
-    function showAllPos(a,b,c){
+    var posShowContent = 1950;
+    function showAllPos(a,b,c,d){
         posShowJoin = a; 
         posShowTarget = b;
         posShowActii = c;
+        posShowContent = d;
     }
     function showSvgIntro(pb){
         if(pb>=253){
@@ -49,7 +58,7 @@
     }
 
     function showContact(pb){
-        if(pb>=1950){
+        if(pb>=posShowContent){
             $(".ctContact").addClass('showContactt'); 
             $(".showContactt").show();   
         }
@@ -65,32 +74,42 @@
 //Responsive screen
     var check=true;
     function checkResize(){
-        console.log($(window).width())
-        if ($(window).width() <= 600) {
+
+        console.log("CHeck resize: "+$(window).width())
+        
+         if ($(window).width() <= 600) {
+            console.log("Thu nho");
             if(check===true){   
+                console.log("Thu nho");
                 // showJoin =500;
                 // showTarget =600;
                 // showActii = 858;
-                showAllPos(500,600,858);
+                showAllPos(500,600,858,1500);
                     check=false;
                     $(".reduceWidth").css('height', '-=280');
-                    //alert('dã tru')   
-            }      
-        }else{
-           
-            if(check===false){
-                showAllPos(850,1100,1562);
-                // showJoin =850;
-                // showTarget =1100;
-                // showActii = 1562;
-                check=true;
-                $(".reduceWidth").css('height', '+=280');
+                    //alert('dã tru')  
+                } 
+            }else if($(window).width() <= 1000){
+                if(check===true){   
+                    showAllPos(0,0,0,0);
+                    showAll(0);
+                }
+            }     
+            else{  
+                if(check===false){
+                        console.log("TO");
+                        showAllPos(850,1100,1562,1950);
+                        // showJoin =850;
+                        // showTarget =1100;
+                        // showActii = 1562;
+                        check=true;
+                        $(".reduceWidth").css('height', '+=280');
         }        
         }
     }
  
     $(window).resize(function(){
         checkResize();
- });
+    });
 
  checkResize();
--- setting.js
+++
@@ -1,9 +0,0 @@
- //ScrollDown introduce
-function scrollToLocation(id){
-            let target = $(id).attr("href");
-            console.log("Top: "+$(target).offset().top);
-            $('html,body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 1000);
-}   
-
